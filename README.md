a3e847cb355e89b4b3fc725bd3fe7734dd8d4357
Ray-Jony <47299859+Ray-Jony@users.noreply.github.com>
2019-08-04 22:06:01 +0100

    Leetcode Exercise

---
+++ README.md
@@ -0,0 +1,1 @@
+## _**This is a project that store my exercise on leetcode**_
---
+++ src/NetEase/Q0004.java
@@ -0,0 +1,11 @@
+package NetEase;
+
+/**
+ * Class Q0004 is created on 02/08/2019 22:47.
+ *
+ * @author Ray
+ * @version 02/08/2019
+ **/
+
+public class Q0004 {
+}
