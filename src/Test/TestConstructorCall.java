3fe192697543acc62ac135f063a6b9c8263c0d02
guorui16 <guorui16@git.xiaomi.com>
2020-06-17 16:57:21 +0800

    another thought
    Signed-off-by: guorui16 <guorui16@git.xiaomi.com>

--- src/Test/TestConstructorCall.java
+++ src/Test/TestConstructorCall.java
@@ -3,6 +3,7 @@ package Test;
 public class TestConstructorCall {
 
     private SomeCallback mSomeCallback;
+    private volatile boolean doCallback = false;
 
     TestConstructorCall(SomeCallback callback) {
         mSomeCallback = callback;
@@ -11,14 +12,23 @@ public class TestConstructorCall {
 
     void callTheCallback() {
         // mSomeCallback.call(); //这样会导致InitCallback里的 testConstructorCall没有初始化就发送信息过去。
-        new Thread(() -> mSomeCallback.call()).start();
+        new Thread(() -> {
+//            while (!doCallback) {
+//            }
+            mSomeCallback.call();
+        }).start();
 
         //- 还是会有Null pointer错误， 所以这样做就是赌这个类会提前比新线程里的call方法提前执行。
 //        try {
-//            Thread.sleep(1000);
+//            Thread.sleep(5000);
+//
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
 //        }
+//        doCallback = true;
+        //- 这就是。。。撒手就跑的行为，Thread.start()后面执行什么都会导致这个类的初始化的比callback更慢
+        // 但做循环测试貌似每次都能跑掉。。。
+        //可能多线程是不能避免的，所以需要notify另一个线程我初始化完了可能是正确的解决方式？
     }
 
     void doSome() {
@@ -26,7 +36,9 @@ public class TestConstructorCall {
     }
 
     public static void main(String[] args) {
-        InitCallback initCallback = new InitCallback();
+        while (true) {
+            new InitCallback();
+        }
     }
 }
 
@@ -40,7 +52,7 @@ class InitCallback {
         }
     };
 
-    InitCallback(){
+    InitCallback() {
         testConstructorCall = new TestConstructorCall(mCallback);
     }
 }
