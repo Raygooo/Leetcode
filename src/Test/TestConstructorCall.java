61c6b5c914265aae4f0e9bec21ab8aecfec105f7
Rui <815098769@qq.com>
2020-06-22 21:13:51 +0800

    some test

--- src/Test/TestConstructorCall.java
+++ src/Test/TestConstructorCall.java
@@ -1,5 +1,7 @@
 package Test;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TestConstructorCall {
 
     private SomeCallback mSomeCallback;
@@ -7,25 +9,29 @@ public class TestConstructorCall {
 
     TestConstructorCall(SomeCallback callback) {
         mSomeCallback = callback;
-        callTheCallback();
     }
 
     void callTheCallback() {
         // mSomeCallback.call(); //这样会导致InitCallback里的 testConstructorCall没有初始化就发送信息过去。
+        ReentrantLock lock = new ReentrantLock();
+        System.out.println(Thread.currentThread().getName());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         new Thread(() -> {
-//            while (!doCallback) {
-//            }
+            System.out.println(Thread.currentThread().getName());
             mSomeCallback.call();
         }).start();
-
         //- 还是会有Null pointer错误， 所以这样做就是赌这个类会提前比新线程里的call方法提前执行。
-//        try {
-//            Thread.sleep(5000);
-//
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        doCallback = true;
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //        doCallback = true;
         //- 这就是。。。撒手就跑的行为，Thread.start()后面执行什么都会导致这个类的初始化的比callback更慢
         // 但做循环测试貌似每次都能跑掉。。。
         //可能多线程是不能避免的，所以需要notify另一个线程我初始化完了可能是正确的解决方式？
@@ -36,9 +42,9 @@ public class TestConstructorCall {
     }
 
     public static void main(String[] args) {
-        while (true) {
-            new InitCallback();
-        }
+
+        new InitCallback().testConstructorCall.callTheCallback();
+
     }
 }
 
