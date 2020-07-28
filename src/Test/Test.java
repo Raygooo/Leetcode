92d203d8a0edbb62d935966b47317bbb9cb39212
guorui16 <guorui16@git.xiaomi.com>
2020-07-28 09:47:37 +0800

    added UUID tools
    Signed-off-by: guorui16 <guorui16@git.xiaomi.com>

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,14 +1,48 @@
 package Test;
 
 import java.awt.*;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Test {
-    public static void main(String[] args) {
-        System.out.println(UUID.randomUUID().toString());
+    public static void main(String[] args) throws IOException {
+//        byte[] a = new byte[]{1,2};
+//        byte[] b = new byte[]{1,2};
+//        byte c = 3;
+//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//        byteArrayOutputStream.write(a);
+//        byteArrayOutputStream.write(b);
+//        byteArrayOutputStream.write(c);
+//        System.out.println(Arrays.toString(byteArrayOutputStream.toByteArray()));
+//        byteArrayOutputStream.close();
+
+        byte[] source = new byte[]{0, 1, 2, 3, 4, 5};
+        byte part1 = source[0];
+        byte[] part2 = new byte[5];
+
+        System.arraycopy(source, 1, part2, 0, part2.length);
+        System.out.println(Arrays.toString(part2));
+
+        byte b = 1;
+        switch (b) {
+            case EnumTest.IDM:
+
+        }
+
+    }
+
+    public enum EnumTest{
+        IDM((byte)1);
+
+        byte aByte;
+        EnumTest(byte b) {
+            aByte = b;
+        }
+
+        public byte getaByte() {
+            return aByte;
+        }
     }
 }
---
+++ src/Util/RandomUUID.java
@@ -0,0 +1,19 @@
+package Util;
+
+import java.util.UUID;
+
+public class RandomUUID {
+
+
+    public static String generateRandomUUID(int length) {
+        System.out.println(UUID.fromString("name"));
+        System.out.println(UUID.fromString("name"));
+        return "";
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(UUID.randomUUID());
+        }
+    }
+}
