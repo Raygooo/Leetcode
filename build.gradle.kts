3050b57686db3d45ae08417f8f1921be0522e3f0
Raym <g1oveg@outlook.com>
2023-08-21 20:48:22 +0800

    Refactor to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.20"
+    application
+}
+
+group = "me.ray"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven{ url = uri("https://maven.aliyun.com/repository/central") }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
